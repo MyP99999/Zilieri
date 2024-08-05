3e0fe89248da3a72f53b7f82394dbef1e605b66d
Matheos Profirescu <matheosprofirescu@yahoo.com>
2024-08-05 13:27:23 +0300

    project setup

---
+++ src/main/java/com/MyP/Zilieri/ZilieriApplication.java
@@ -0,0 +1,13 @@
+package com.MyP.Zilieri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZilieriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZilieriApplication.class, args);
+	}
+
+}
